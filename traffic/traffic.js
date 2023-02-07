36a1086bfefc42622fd73c4d2b5685a69bb939b2
hothikieu2811 <hothikieu28112003@gmail.com>
2023-02-07 15:34:55 +0700

    update

---
+++ traffic/traffic.js
@@ -0,0 +1,10 @@
+function getGreenColor() {
+    document.getElementById("Myimge").src = "/img/greenlight.png";
+}
+function getYellowColor() {
+    document.getElementById("Myimge").src = "/img/yellowlight.png";
+}
+function getRedColor() {
+    document.getElementById("Myimge").src = "/img/redlight.png";
+}
+
